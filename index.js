8b6b41c8755ff1e7d0e5d8df8e1527828f31ad8f
mrana565 <rana565m@gmail.com>
2023-05-04 18:25:15 +0600

    recipes app

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const chefs = require("./chifs.json");
+const recipes = require("./recipes.json");
 
 app.use(cors());
 
-// app.get("/", (req, res) => {
-//   res.send("express js is running");
-// });
-
 app.get("/chefs", (req, res) => {
   res.send(chefs);
+});
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
 });
 
 app.listen(port, () => {
